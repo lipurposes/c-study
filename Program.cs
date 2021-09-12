ca21e69f70061f3bcf29f69ab668721c985e2d81
lipengpeng <lipurpose@aliyun.com>
2021-09-12 11:36:31 +0800

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] numArray = { new int[]{1}, new int[]{3,4}, new int[]{5,6,7} };
+        }
+    }
+}
