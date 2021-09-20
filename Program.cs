e27ae4be4bf5bea070ef6af1146b3ba60019ffcf
lipengpeng <lipurpose@aliyun.com>
2021-09-20 12:16:55 +0800

    类学习1

--- Program.cs
+++ Program.cs
@@ -2,12 +2,52 @@
 
 namespace firstStudy
 {
+    class Persion
+    {
+        private int m_age;
+        public int age
+        {
+            get
+            {
+                return m_age;
+            }
+            set
+            {
+                m_age = value;
+            }
+        }
+        public string Name { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            int[][] numArray = { new int[]{1}, new int[]{3,4}, new int[]{5,6,7} };
+            int[,] numArray1 = { { 2, 3 }, { 4, 5 } };
+            int[,] numArray2 = new int[,] { { 8, 9 }, { 10, 11 } };
+            int length = numArray1.Length;
+            int firstLength = numArray1.GetLength(0);
+            int[][] numArray = { new int[] { 1 }, new int[] { 3, 4 }, new int[] { 5, 6, 7 } };
             Console.WriteLine("Hello World");
+            int total = Sum(5, 6, 7);
+
+            void Method(ref int refArgument)
+            {
+                refArgument = refArgument + 44;
+            }
+
+            int number = 1;
+            Method(ref number);
+
+        }
+
+        static int Sum(params int[] nums)
+        {
+            int sum = 0;
+            foreach (var x in nums)
+            {
+                sum += x;
+            }
+            return sum;
         }
     }
 }
